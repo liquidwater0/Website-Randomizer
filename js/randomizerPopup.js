a4057a0d4f909323ec022a93d66bda2127a60dea
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-06-12 19:08:55 -0500

    new primary color changer

--- js/randomizerPopup.js
+++ js/randomizerPopup.js
@@ -14,14 +14,19 @@ document.getElementById("optionsButton").addEventListener("click", function() {
     }
 });
 
+function convertHexToRGB(hex) {
+    const hex_code = hex.split("");
+    const red = parseInt(hex_code[1]+hex_code[2],16);
+    const green = parseInt(hex_code[3]+hex_code[4],16);
+    const blue = parseInt(hex_code[5]+hex_code[6],16);
+    const rgb = `${red}, ${green}, ${blue}`;
+
+    return rgb;
+}
+
 function get() {
-    chrome.storage.sync.get({currentTheme: "dark", primaryColorText: "0, 160, 255"}, function(items) {
+    chrome.storage.sync.get({currentTheme: "dark", primaryColor: "#00a0FF"}, function(items) {
         html.setAttribute("data-theme", items.currentTheme);
-
-        document.documentElement.style.setProperty("--primaryColor", items.primaryColorText);
-
-        if (items.primaryColorText == "") {
-            document.documentElement.style.setProperty("--primaryColor", "0, 160, 255");
-        }
+        document.documentElement.style.setProperty("--primaryColor", convertHexToRGB(items.primaryColor));
     });
 }
