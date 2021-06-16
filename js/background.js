6fa0394c18d1e50ff064413f7914dcdff4ff85aa
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-06-15 19:25:10 -0500

    remove background.js from js folder cause of manifest 3

--- js/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(function() {
-	if (chrome.runtime.openOptionsPage) {
-	 chrome.runtime.openOptionsPage();
-	} else {
-	 window.open(chrome.runtime.getURL("randomizerOptions.html"));
-	}
-});
