8ad7887d7f3b28fc64e5d4abeea32bc5d6502382
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-04-25 18:40:28 -0500

    random date function and rgb to hex function

--- js/content/utilities.js
+++ js/content/utilities.js
@@ -1,7 +1,3 @@
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 let delay;
 let checkStatesMap = new Map();
 
@@ -30,4 +26,48 @@ function getEnabled(checkbox) {
 function randomStyle(property, value) {
     const everything = document.querySelectorAll("body *");
     everything.forEach(element => element.style[property] = value());
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//https://www.w3docs.com/snippets/javascript/how-to-convert-rgb-to-hex-and-vice-versa.html
+function rgbToHex(red, green, blue) {
+    return `#${((1 << 24) + (red << 16) + (green << 8) + blue).toString(16).slice(1)}`;
+}
+
+function getRandomDate() {
+    const randomMonth = getRandomNumber(1, 12);
+    const randomDay = getRandomNumber(1, 31);
+    const randomYear = getRandomNumber(1900, 2100);
+    const randomWeek = getRandomNumber(1, 52);
+
+    const randomHour = getRandomNumber(0, 23);
+    const randomMinutes = getRandomNumber(0, 59);
+    const randomSeconds = getRandomNumber(0, 59);
+    const randomMilliseconds = getRandomNumber(0, 999);
+    
+    const randomDateISO = new Date(`${randomYear}-${randomMonth}-${randomDay}`);
+
+    const month = String(randomDateISO.getMonth() + 1).padStart(2, "0");
+    const day = String(randomDateISO.getDate()).padStart(2, "0");
+    const year = String(randomDateISO.getFullYear());
+    const week = String(randomWeek).padStart(2, "0");
+
+    const hour = String(randomHour).padStart(2, "0");
+    const minutes = String(randomMinutes).padStart(2, "0");
+    const seconds = String(randomSeconds).padStart(2, "0");
+    const milliseconds = String(randomMilliseconds).padStart(3, "0");
+
+    return {
+        month: month,
+        day: day,
+        year: year,
+        week: week,
+        hour: hour,
+        minutes: minutes,
+        seconds: seconds,
+        milliseconds: milliseconds
+    }
 }
