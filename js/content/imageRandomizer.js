468eeb9db733703f3493720b5d61f8aaf0df5f8a
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-05-14 18:06:48 -0500

    live randomization!

--- js/content/imageRandomizer.js
+++ js/content/imageRandomizer.js
@@ -1,27 +1,24 @@
-chrome.storage.sync.get({}, items => {
-    setTimeout(imageRandomizer, delay);
+function imageRandomizer(nodes) {
+    if (!getEnabled("imageEnabled")) return;
+    
+    const images = nodes.images;
 
-    function imageRandomizer() {
-        if (!getEnabled("imageEnabled")) return;
+    if (getEnabled("randomImages")) {
+        images.forEach(image => {
+            const randomImage = staticImages[Math.floor(Math.random() * staticImages.length)];
+            const { src, srcset, alt } = randomImage;
 
-        const images = document.querySelectorAll("img");
-
-        if (getEnabled("randomImages")) {
-            images.forEach(image => {
-                const randomImage = images[Math.floor(Math.random() * images.length)];
-
-                image.src = randomImage.src;
-                image.srcset = randomImage.srcset;
-                image.alt = randomImage.alt;
-            });
-        }
-
-        if (getEnabled("randomImageHeight")) {
-            images.forEach(image => image.height = getRandomNumber(0, 500));
-        }
-
-        if (getEnabled("randomImageWidth")) {
-            images.forEach(image => image.width = getRandomNumber(0, 500));
-        }
+            image.src = src;
+            image.srcset = srcset;
+            image.alt = alt;
+        });
     }
-});
+
+    if (getEnabled("randomImageHeight")) {
+        images.forEach(image => image.height = getRandomNumber(0, 500));
+    }
+
+    if (getEnabled("randomImageWidth")) {
+        images.forEach(image => image.width = getRandomNumber(0, 500));
+    }
+}
