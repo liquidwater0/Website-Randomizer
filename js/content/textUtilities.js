d4b8f962378d67d6941f037526e7fffc72970be1
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-05-14 18:06:08 -0500

    live randomization!

--- js/content/textUtilities.js
+++ js/content/textUtilities.js
@@ -1,49 +1,13 @@
 let singleWords;
+//This is here so check states load in properly
+chrome.storage.sync.get({}, items => singleWords = getEnabled("singleWords"));
 
-chrome.storage.sync.get({}, items => {
-    singleWords = getEnabled("singleWords"); //put single words variable here so it actually gets set properly
-    setTimeout(getTextNodes, delay); //put timeout here so it gets delay variable
-});
-
-let staticText = [];
-let textNodes = [];
-
-function getTextNodes() {
-    const treeWalker = document.createTreeWalker(
-        document.body, 
-        NodeFilter.SHOW_TEXT,
-        {
-            acceptNode: node => {
-                if (
-                    node.parentElement.tagName === "SCRIPT" ||
-                    node.parentElement.tagName === "STYLE" ||
-                    node.parentElement.tagName === "NOSCRIPT" || //Some sites have html in noscript and I don't want that showing up.
-                    node.nodeValue.trim() === ""
-                ) return NodeFilter.FILTER_SKIP;
-
-                return NodeFilter.FILTER_ACCEPT;
-            }
-        }
-    );
-    
-    let currentNode = treeWalker.currentNode;
-    
-    while (currentNode) {
-        staticText.push(currentNode.nodeValue);
-        textNodes.push(currentNode);
-        currentNode = treeWalker.nextNode();
-    }
-
-    staticText.shift();
-    textNodes.shift();
+function randomizeText(nodes) {
+    nodes.forEach(node => node.nodeValue = getRandomText());
 }
 
-function randomizeText() {
-    textNodes.forEach(node => node.nodeValue = getRandomText());
-}
-
-function shuffleText() {
-    textNodes.forEach(node => node.nodeValue = getShuffledText(node.nodeValue));
+function shuffleText(nodes) {
+    nodes.forEach(node => node.nodeValue = getShuffledText(node.nodeValue));
 }
 
 function getRandomText() {
@@ -63,7 +27,8 @@ function getShuffledText(text) {
     const shuffledText = words
         .map(value => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value);
+        .map(({ value }) => value)
+        .join(" ");
 
-    return shuffledText.join(" ");
+    return shuffledText;
 }
