c362f9adc21e136ef7aaacafcbb8c960f057dc3a
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-03-29 19:33:41 -0500

    handle newly added and removed checkboxes

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,18 +1,20 @@
-import { imageCheckboxes, textCheckboxes, elementCheckboxes, optionsCheckboxes } from "../../checkboxes";
+import { 
+	imageCheckboxes, 
+	textCheckboxes, 
+	elementCheckboxes, 
+	optionsCheckboxes, 
+	CheckboxOption 
+} from "../../checkboxes";
 
-function saveOptions() {
+chrome.runtime.onInstalled.addListener(details => {
+    if (details.reason !== "install") return;
+
 	chrome.storage.sync.set({
 		imageCheckedStates: imageCheckboxes, 
 		textCheckedStates: textCheckboxes,
 		elementCheckedStates: elementCheckboxes,
 		optionsCheckedStates: optionsCheckboxes
 	});
-}
-
-chrome.runtime.onInstalled.addListener(details => {
-    if (details.reason !== "install") return;
-
-	saveOptions();
 
     if (chrome.runtime.openOptionsPage) {
 		chrome.runtime.openOptionsPage();
@@ -20,3 +22,38 @@ chrome.runtime.onInstalled.addListener(details => {
 		window.open(chrome.runtime.getURL("options.html"));
 	}
 });
+
+handleUpdatedCheckboxes();
+
+function handleUpdatedCheckboxes() {
+	setUpdatedArray(imageCheckboxes, "imageCheckedStates");
+	setUpdatedArray(textCheckboxes, "textCheckedStates");
+	setUpdatedArray(elementCheckboxes, "elementCheckedStates");
+	setUpdatedArray(optionsCheckboxes, "optionsCheckedStates");
+}
+
+function setUpdatedArray(randomizerCheckboxes: CheckboxOption[], checkedStatesStorageKey: string) {
+	chrome.storage.sync.get({ [checkedStatesStorageKey]: [] }, items => {
+		let finalArray: CheckboxOption[] = items[checkedStatesStorageKey];
+
+		randomizerCheckboxes.forEach(checkbox => {
+			const newCheckbox: CheckboxOption | undefined = items[checkedStatesStorageKey].find((item: CheckboxOption) => {
+				return item.id === checkbox.id;
+			});
+	
+			if (newCheckbox === undefined) finalArray.push(checkbox);
+		});
+	
+		items[checkedStatesStorageKey].forEach((checkbox: CheckboxOption) => {
+			const removedCheckbox: CheckboxOption | undefined = randomizerCheckboxes.find((item: CheckboxOption) => {
+				return item.id === checkbox.id;
+			});
+	
+			if (removedCheckbox === undefined) {
+				finalArray = finalArray.filter(item => item.id !== checkbox.id);
+			}
+		});
+	
+		chrome.storage.sync.set({ [checkedStatesStorageKey]: finalArray });
+	});
+}
